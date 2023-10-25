7ae1f52c50dfa49a0bc555a47290ca19ab2a5391
Josh Wedekind <josh@halfnibble.com>
2023-10-24 21:28:50 -0700

    Use env vars for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+    const [message, setMessage] = useState("Loading...");
+    const [secret, setSecret] = useState("Loading...");
+
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+    useEffect(() => {
+        axios.get(`${API_URL}/`).then((resp) => {
+            setMessage(resp.data.message);
+            setSecret(resp.data.secret);
+        });
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Test</h1>
+                <h3>{message}</h3>
+                <h4>Don't tell anyone this: {secret}</h4>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
