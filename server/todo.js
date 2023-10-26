63fa33822b29e7d8898b753c5cb18a77bed47132
Josh Wedekind <josh@halfnibble.com>
2023-10-26 01:08:42 -0700

    Add order

--- server/todo.js
+++ server/todo.js
@@ -2,8 +2,13 @@ const client = require("./db-client");
 
 async function listTodosForUser(token) {
     const result = await client.query(
-        'SELECT t.id, t.name, t.completed FROM "Todo" t ' +
-            'JOIN "User" u ON t.user_id = u.id WHERE u.token = $1',
+        `
+            SELECT "Todo".id, "Todo".name, "Todo".completed 
+            FROM "Todo"
+            JOIN "User" ON "Todo".user_id = "User".id 
+            WHERE "User".token = $1
+            ORDER BY "Todo".id
+        `,
         [token]
     );
     return result.rows;
@@ -11,27 +16,41 @@ async function listTodosForUser(token) {
 
 async function createTodoForUser(token, name) {
     const result = await client.query(
-        'INSERT INTO "Todo" (name, user_id) ' +
-            'SELECT $1, u.id FROM "User" u WHERE u.token = $2 ' +
-            "RETURNING id, name, completed",
+        `
+            INSERT INTO "Todo" (name, user_id)
+            SELECT $1, "User".id FROM "User" WHERE "User".token = $2
+            RETURNING "Todo".id, "Todo".name, "Todo".completed
+        `,
         [name, token]
     );
     return result.rows[0]; // Return the newly created Todo item
 }
 
-async function updateTodoItem(token, todoId, updatedName, updatedStatus) {
+async function updateTodoItem(token, todoId, updatedName, updatedCompleted) {
     const result = await client.query(
-        'UPDATE "Todo" SET name = $1, completed = $2 ' +
-            'FROM "User" WHERE "Todo".id = $3 AND "User".token = $4 ' +
-            'RETURNING "Todo".id, name, completed',
-        [updatedName, updatedStatus, todoId, token]
+        `
+            UPDATE "Todo"
+            SET name = $1, completed = $2
+            FROM "User"
+            WHERE "Todo".id = $3
+            AND "User".token = $4
+            AND "Todo".user_id = "User".id
+            RETURNING "Todo".id, "Todo".name, "Todo".completed
+        `,
+        [updatedName, updatedCompleted, todoId, token]
     );
     return result.rows[0]; // Return the updated Todo item
 }
 
 async function deleteTodoItem(token, todoId) {
     await client.query(
-        'DELETE FROM "Todo" ' + 'USING "User" WHERE "Todo".id = $1 AND "User".token = $2',
+        `
+            DELETE FROM "Todo"
+            USING "User"
+            WHERE "Todo".id = $1 
+            AND "User".token = $2 
+            AND "Todo".user_id = "User".id
+        `,
         [todoId, token]
     );
     // No return value, you can check if deletion was successful by counting affected rows if needed
